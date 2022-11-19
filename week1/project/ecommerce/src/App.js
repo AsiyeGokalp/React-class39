acb73d8a5bee0bcdf6a05e049e2c5f9b31078a1e
Asiye <gokalpasiye@gmail.com>
2022-11-19 02:43:42 +0100

    some try for fix

--- week1/project/ecommerce/src/App.js
+++ week1/project/ecommerce/src/App.js
@@ -6,8 +6,10 @@ import "./App.css"
 
 function App() {
   const [productList, setProductList] = useState(ALL_PRODUCTS)
+  const [activeButton, setActiveButton] = useState(0)
+
   const filterByCategory = (category) => {
-    const newProductList = productList.filter(
+    const newProductList = ALL_PRODUCTS.filter(
       (item) => `FAKE: ${item.category}` === category
     )
     setProductList(newProductList)
@@ -17,13 +19,17 @@ function App() {
     <div className="App">
       <h1>Products</h1>
 
-      {ALL_CATEGORIES.map((item) => {
-        return <button onClick={() => filterByCategory(item)}>{item}</button>
+      {ALL_CATEGORIES.map((item, index) => {
+        return <button
+          key={index}
+          onClick={() => { filterByCategory(item); setActiveButton(index) }}
+          className={activeButton === index ? "active" : "non-active"}
+        >{item}</button>
       })}
 
       <div className="gallery">
-        {productList.map((product) => {
-          return <Product {...product} />
+        {productList.map((product, index) => {
+          return <Product key={index} {...product} />
         })}
       </div>
     </div>
