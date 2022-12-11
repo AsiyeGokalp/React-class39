55fac9409b44c8805a2a5021d88e7a9c0a7b0a0f
AsiyeGokalp <107327598+AsiyeGokalp@users.noreply.github.com>
2022-12-11 21:02:58 +0100

    Update App.js

--- week3/project/ecommerce/src/App.js
+++ week3/project/ecommerce/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import ProductList from './ProductList';
 import ProductDetail from './ProductDetail';
 import { Routes, Route } from 'react-router-dom'
 import FavoriteProducts from './favoritedProducts'
-import Navigation from './Navigation';
+import Navigation from './navigation';
 import { FavoriteProductProvider } from './favoriteProductContext'
 
 
